d49b3404a3f55437fdeceb2745ad7ddb1c65a188
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-01-01 03:05:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+document.querySelector('input[type="button"]').addEventListener('click', function(){
+	const color = document.getElementById('colorSelect');
+
+	if(color.selectedIndex!==-1){
+		color.remove(color.selectedIndex);
+	}
+})
